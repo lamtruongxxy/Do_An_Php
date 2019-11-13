291df7ebadc25e59a09d643acb8eaa543298e624
anhtu04091999 <55349397+anhtu04091999@users.noreply.github.com>
2019-11-13 12:57:30 +0700

    Thay đổi foler Lưu

--- doan_PHP/app/Http/Controllers/API/LinhVucAPI.php
+++ doan_PHP/app/Http/Controllers/API/LinhVucAPI.php
@@ -5,14 +5,15 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\LinhVuc;
+
 class LinhVucAPI extends Controller
 {
     public function layDanhSach()
     {
     	$listLinhVuc =LinhVuc::all();
     	$result=[
-    		'success' => true,
-    		'data'    => $listLinhVuc
+    		'success' => true;
+    		'data'    => $listLinhVuc;
     	];
     	return response()->json($result);
     }
