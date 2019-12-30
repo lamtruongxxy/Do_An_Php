4a71754b6b1a270557cb92efa4da66a352b48fac
lamtruongxxy <55741716+lamtruongxxy@users.noreply.github.com>
2019-12-30 21:38:45 +0700

    V17.2 fix api đăng nhập

--- doan_PHP/app/Http/Controllers/API/LoginController.php
+++ doan_PHP/app/Http/Controllers/API/LoginController.php
@@ -15,27 +15,30 @@ class LoginController extends Controller
     public function dangNhap(Request $req)
     {
     	//lay thong tin Nguoi choi cần dùng cho đăng nhập
-    	$credentials = [
-    		'ten_dang_nhap' => $req->ten_dang_nhap,
-    		'password' => $req->mat_khau,
-    	];
+    	$credentials = $req->only('ten_dang_nhap','password');
+    	$token = auth('api')->attempt($credentials);
     	// chứng thực thất bại
-    	if(!$token = auth('api')->attempt($credentials))
+    	if(!$token)
     	{
+
     		// nếu sai ten dang nhap hoac mat khau
-    		return response()->json([
+    		$res=[
     			'status'  => false,
     			'message' => 'Đăng nhập không thành công '// khong dươc phép
-    		], 401);
+    		];
+    		return response()->json($res);
     	}
+    	$user = auth('api')->user(); 
     	// chứng thực thành công
-    	return response()->json([
+    	$res=[
     		'status'  => true,
     		'message' =>'Đăng nhập thành công',
+    		'user' 	  =>$user,
     		'token'   => $token,
     		'type'    => 'bearer', //có thể bỏ
-    		'expires' => auth('api')->factory()->getTTL() * 30
-    	], 200); //có thể bỏ
+    		'expires' => auth('api')->factory()->getTTL() * 60 * 24 * 7
+    	]; //có thể bỏ
+    	return response()->json($res);
 	}
 	
 	public function dangXuat()
