6007658e8e71ec7d57ef841cb852c1da189e0cea
khanhs1412 <50403309+khanhs1412@users.noreply.github.com>
2020-01-01 20:09:01 +0700

    V21 Update API Câu Hỏi

--- doan_PHP/app/Http/Controllers/API/CauHoiAPI.php
+++ doan_PHP/app/Http/Controllers/API/CauHoiAPI.php
@@ -8,23 +8,41 @@ use App\CauHoi;
 
 class CauHoiAPI extends Controller
 {
-    public function layDanhSachCauHoi() 
-    {
-        $dsCauHoi = CauHoi::all();
-        $ketqua = [
-                'success' => true,
-                'data'    => $dsCauHoi,
-            ];
-            return response()->json($ketqua);
-    }
+    // public function layDanhSachCauHoi() 
+    // {
+    //     $dsCauHoi = CauHoi::all();
+    //     $ketqua = [
+    //             'success' => true,
+    //             'data'    => $dsCauHoi,
+    //         ];
+    //         return response()->json($ketqua);
+    // }
 
-    public function layMotCauHoi($id)
+    // public function layMotCauHoi($id)
+    // {
+    //     $dsCauHoi = CauHoi::find($id);
+    //     $ketqua = [
+    //             'success' => true,
+    //             'data'    => $dsCauHoi,
+    //         ];
+    //         return response()->json($ketqua);
+    // }
+     public function cauHoiTheoLinhVuc($id)
     {
-        $dsCauHoi = CauHoi::find($id);
-        $ketqua = [
-                'success' => true,
-                'data'    => $dsCauHoi,
+        $cauhois=CauHoi::where('linh_vuc_id',$id)->get();
+        if (count($cauhois) === 0)
+        {
+            $res = [
+                'success'   => false,
+                'msg'       => 'Không tìm thấy câu hỏi tương ứng'
             ];
-            return response()->json($ketqua);
+            return response()->json($res);
+        }
+        $res = [
+            'success'   => true,
+            'msg'       => 'Tải câu hỏi thành công',
+            'data'      => $cauhois
+        ];
+        return response()->json($res);
     }
 }
